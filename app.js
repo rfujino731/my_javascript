5e1aa00ac0561a83669a52a144ec41d8c13e9d2b
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-05-11 18:13:10 +0900

    [finish]6-5

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-$(document).ready(function () {
-	// jQueryプログラムの内容
-    $('body').html('<h1>Hello jQuery!!</h1>');
-});
+// $(document).ready(function () {
+// 	// jQueryプログラムの内容
+//     $('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+
+ $(function(){
+        $('.box1').slideDown(function() {
+            $('.box1').css({
+                'background-color': '#0000FF',
+                'width': '200px',
+                'height': '100px'
+            }).slideUp();
+        });
+    });
