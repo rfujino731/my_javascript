e3a9731eaeb6a20148a92ff298e0f7a5f612178c
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-05-12 00:36:10 +0900

    [finish]6-6

--- app.js
+++ app.js
@@ -4,12 +4,43 @@
 // });
 
 
- $(function(){
-        $('.box1').slideDown(function() {
-            $('.box1').css({
-                'background-color': '#0000FF',
-                'width': '200px',
-                'height': '100px'
-            }).slideUp();
-        });
+ // $(function(){
+ //        $('.box1').slideDown(function() {
+ //            $('.box1').css({
+ //                'background-color': '#0000FF',
+ //                'width': '200px',
+ //                'height': '100px'
+ //            }).slideUp();
+ //        });
+ //    });
+
+// $(function(){
+//     $('.box1').mouseover(function(){
+//         $('.box1').css({'background-color': '#0000FF'});
+//     });
+
+//      $('.box1').mouseout(function(){
+//         $('.box1').css({'background-color': '#FF0000'});
+//     });
+// });
+
+// $(function(){
+//     $('.box1').mouseover(function(){
+//         $('.box1').addClass('box1-ext');
+//     });
+ 
+//     $('.box1').mouseout(function(){
+//         $('.box1').removeClass('box1-ext');
+//     });
+// });
+
+$(function(){
+    $('.box1').on('click', function(){
+        $('.box1').addClass('box1-ext');
     });
+ 
+    $('.box1').mouseout(function(){
+        $('.box1').removeClass('box1-ext');
+    });
+ 
+});
